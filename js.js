2229091d53700af26493105335dc79a10c92eaf8
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-03-09 15:55:39 -0300

    correção javascript

--- js.js
+++ js.js
@@ -1,17 +1,17 @@
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-});
-// move next carousel
-$('.moveNextCarousel').click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $('.carousel').carousel('next');
-});
-
-// move prev carousel
-$('.movePrevCarousel').click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $('.carousel').carousel('prev');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel.carousel-slider');
+    var instance = M.Carousel.init(elems, ({
+        fullWidth: true,
+        indicators: true,
+        duration: 200
+    }))
+    var carousel_instance = instance[0];
+    var move_next = document.querySelector('.moveNextCarousel');
+    move_next.addEventListener('click', function(){
+        carousel_instance.next(1)
+    })
+    var move_prev = document.querySelector('.movePrevCarousel');
+    move_prev.addEventListener('click', function(){
+        carousel_instance.prev();
+    })
+  });
