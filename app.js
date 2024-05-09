59b4dc6289e2c3708d43bc938d0a62ee08205c2f
AbnerCerqueira <abner.cerqueira@fatec.sp.gov.br>
2024-05-09 18:12:10 -0300

    style: pasta dao

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const user = require('./dao/user-dao')
 const app = express()
 
 app.set("view engine", "ejs")
@@ -16,7 +17,7 @@ app.get("/login", (req, res) => {
     res.render("login.ejs")
 })
 
-app.use(express.static('static'))
+app.use(express.static('public'))
 
 app.listen(port = 8080, () => {
     console.log(`Iniciando servidor na porta ${port}`)
